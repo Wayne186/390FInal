68e892e7f13ed5516b59f76cfc561921284788ec
Jing <zheng276@purdue.edu>
2018-04-12 17:53:58 -0400

    set env and working on longbow

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Arrow : MonoBehaviour {
+
+	private bool isAttached = false;
+
+	private bool isShot = false;
+
+	void OnTriggerStay() {
+		AttachArrow ();
+	}
+
+	void OnTriggerEnter() {
+		AttachArrow ();
+	}
+
+	void Update() {
+		if (isShot && transform.GetComponent<Rigidbody> ().velocity.magnitude > 5f) {
+			transform.LookAt (transform.position + transform.GetComponent<Rigidbody> ().velocity);
+		}
+	}
+
+	public void Shot() {
+		isShot =  true; 
+	}
+
+	private void AttachArrow() {
+		var device = SteamVR_Controller.Input((int)ArrowManager.Instance.trackedObj.index);
+		if (!isAttached && device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			ArrowManager.Instance.AttachBowToArrow ();
+			isAttached = true;
+		}
+	}
+
+
+}
